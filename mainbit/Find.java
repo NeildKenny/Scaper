7e70b64df6189c6239ad6fbbc1b11c28e892c33a
Neil Kenny <neildkenny@gmail.com>
2017-03-18 15:00:01 +0000

    First commit

---
+++ mainbit/Find.java
@@ -0,0 +1,113 @@
+package mainbit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Find {
+
+	public ArrayList<String> findFirst( ArrayList<String> elements, String find_element ){
+		
+		ArrayList<String> element_list = new ArrayList<String>();
+
+		String find_element_end = "</" + find_element.substring(1, find_element.length()) + ">";
+
+		int elements_found = 0;
+		int first_position = 0; 
+		int last_position = 0;
+		
+		
+		for( int i = 0; i<elements.size(); i++ ){
+			
+			if(elements.get(i).contains(find_element)){
+				elements_found++;
+				
+				if(first_position == 0){
+					first_position = i;
+				}
+				
+			}
+			
+			if(first_position != 0){
+				element_list.add(elements.get(i));
+				//System.out.print(elements.get(i) + "\n");
+
+			}
+			
+			if( elements.get(i).contains(find_element_end) && first_position != 0 ){
+				elements_found--;
+				if(elements_found == 0){
+					last_position = i; 	
+					break;
+				}
+			}
+			
+		}
+
+		
+		for(int i = 0; i<element_list.size();i++){
+			System.out.println(element_list.get(i));
+		}
+		
+		System.out.println("Element found: " 	+ elements_found + "\n" 
+						+ "First position: " 	+ first_position + "\n"
+						+ "Last position: " 	+ last_position + "\n");
+
+		return element_list;
+		
+		
+	}
+	
+	public List<ArrayList<String>> findAllOccurances( ArrayList<String> elements, String find_element ){
+		List<ArrayList<String>> elements_list = new ArrayList<ArrayList<String>>();
+		
+		
+		String find_element_end = "</" + find_element.substring(1, find_element.length()) + ">";
+		System.out.println(find_element_end);
+		
+		int elements_found = 0;
+		int first_position = 0; 
+		int last_position = 0;
+		
+		int first_count = 0; 
+		int second_count = 0;
+
+		for( int i = 0; i<elements.size(); i++ ){
+			if( elements.get(i).contains(find_element) ){
+				elements_found++; 
+				elements_list.add(new ArrayList<String>());
+			
+				first_count++;
+			}
+			
+			if( elements_found !=0 ){
+				for( int j=0; j<elements_found; j++ ){
+					elements_list.get(j).add(elements.get(i));
+				}
+			}
+			if( elements.get(i).contains(find_element_end) && elements_found !=0 ){
+				elements_found--;
+				second_count++;
+			}
+		}
+		
+		
+		
+		/*for(int i= 0; i<elements_list.size(); i++){
+			
+			System.out.println("----------------------------------------------------------------");
+			for(int j=0; j<elements_list.get(i).size(); j++){
+				System.out.println( elements_list.get(i).get(j) );
+			}
+		}*/
+		System.out.println("Element found: " 	+ elements_found + "\n" 
+				+ "First position: " 	+ first_position + "\n"
+				+ "Last position: " 	+ last_position + "\n"
+				+ "Element list: " + elements_list.size() + "\n"
+				+ "first count" + first_count +  "\n"
+				+ "second count" + second_count
+				);
+		return elements_list; 
+		
+	}
+	
+}
---
+++ mainbit/httptest.java
@@ -0,0 +1,51 @@
+package httpbits;
+
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import scraper.Find;
+
+public class httptest {
+
+	private final String USER_AGENT = "Mozilla/5.0";
+	
+	
+	public void sendGET(String url) throws IOException{
+		URL url_obj;
+		
+		url_obj = new URL(url);
+
+		HttpURLConnection conn = (HttpURLConnection) url_obj.openConnection();
+		
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("User-Agent", USER_AGENT );
+		
+		int response_code = conn.getResponseCode(); 
+		
+		System.out.println(response_code);
+		
+		BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream()));
+		ArrayList<String> site_as_string = new ArrayList<String>();
+		String line;
+		while((line = in.readLine())!=null){
+			site_as_string.add(line);
+		}
+		in.close();
+		
+		Find find = new Find();
+		find.findAllOccurances (site_as_string, "<div");
+		
+		
+	
+	
+	}
+	
+
+	
+}
